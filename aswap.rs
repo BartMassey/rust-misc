0dcc6b27d94402713aede4fad35ac44d471ab24f
Bart Massey <bart@cs.pdx.edu>
2018-04-17 17:59:28 -0700

    applied license to sources

--- aswap.rs
+++ aswap.rs
@@ -1,3 +1,8 @@
+// Copyright © 2018 Bart Massey
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
 fn main() {
     let mut a = [1,2,3];
     let t = a[0];
