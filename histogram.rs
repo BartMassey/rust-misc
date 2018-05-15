7ccd8517107b6824306fad056376724fe07656d2
Bart Massey <bart@cs.pdx.edu>
2018-05-15 13:58:40 -0700

    added second histogram example

--- histogram.rs
+++ histogram.rs
@@ -6,7 +6,9 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::iter::FromIterator;
+use std::fmt;
 
+#[derive(Clone, Debug)]
 struct Histogram<T: Hash + Eq>(HashMap<T, usize>);
 
 impl<T: Hash + Eq> Histogram<T> {
@@ -33,14 +35,32 @@ impl<T: Hash + Eq> FromIterator<T> for Histogram<T> {
     }
 }
 
-fn main() {
-    let alphas = "hello world"
-        .chars()
-        .filter(|c| c.is_alphabetic());
-    let Histogram(h) = alphas.collect();
-    let mut keys: Vec<&char> = h.keys().collect();
-    keys.sort();
-    for key in keys {
-        println!("{}: {}", key, h[key]);
+impl<T> fmt::Display for Histogram<T>
+    where T: Hash + Eq + Ord + fmt::Debug
+{
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        if self.0.is_empty() {
+            return write!(fmt, "Histogram {{}}");
+        }
+        let mut keys: Vec<&T> = self.0.keys().collect();
+        let mut items = Vec::new();
+        keys.sort();
+        for key in keys {
+            items.push(format!("{:?}: {}", key, self.0[key]));
+        }
+        write!(fmt, "Histogram {{{}}}", items.join(", "))
     }
+}
+
+fn main() {
+    let h: Histogram<char> = "hello world"
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect();
+    println!("{}", h);
+
+    let words: Histogram<_> = "a banana is a banana is a banana"
+        .split_whitespace()
+        .collect();
+    println!("{}", words);
 }
